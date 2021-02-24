df3f06a5eae5faf9de90c4551129b25f5bf18feb
LeeThanhBao <bao.lt5030@sinhvien.hoasen.edu.vn>
2021-02-24 09:27:32 +0700

    24/2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: 'note-page', loadChildren: () => import('./shared/page/note-page/note-page.module').then(m => m.NotePageModule) },
-  { path: 'trash-page', loadChildren: () => import('./shared/page/trash-page/trash-page.module').then(m => m.TrashPageModule) }];
+  { path: 'trash-page', loadChildren: () => import('./shared/page/trash-page/trash-page.module').then(m => m.TrashPageModule) },
+  { path: 'signin-page', loadChildren: () => import('./shared/page/signin-page/signin-page.module').then(m => m.SigninPageModule) },
+  { path: 'register-page', loadChildren: () => import('./shared/page/register-page/register-page.module').then(m => m.RegisterPageModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAPUq073NCzk_Cw2ex0yIzNnHSyacb_5eo",
+    authDomain: "memos-a4a21.firebaseapp.com",
+    projectId: "memos-a4a21",
+    storageBucket: "memos-a4a21.appspot.com",
+    messagingSenderId: "425019375866",
+    appId: "1:425019375866:web:b15b9b8016300549998943",
+    measurementId: "G-TFK0VLYVSV"
+  }
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
