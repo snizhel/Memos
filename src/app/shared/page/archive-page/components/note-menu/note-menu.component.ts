ca0cec926b35c2249b6466434bfd09bc6a90a5a5
Thien <bananakind123@gmail.com>
2021-02-25 10:05:03 +0700

    add change img in archive

--- src/app/shared/page/archive-page/components/note-menu/note-menu.component.ts
+++ src/app/shared/page/archive-page/components/note-menu/note-menu.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Note } from 'src/app/models/note-model';
+import { NoteService } from 'src/app/services/note.service';
 
 @Component({
   selector: 'app-note-menu',
@@ -6,10 +9,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./note-menu.component.scss']
 })
 export class NoteMenuComponent implements OnInit {
+  @Input() note: Note;
+  @Input() details: boolean; // means that menu not in noteComponent
+  @Input() newNote: boolean; // means that menu in newPageComponent
+  selecetdFile: File;
+  menuActive: boolean = false; // means that one of menu item open
 
-  constructor() { }
+  constructor(private noteService: NoteService,
+    public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
+  // Image upload event handler
+  onFileUpload(event) {
+    this.selecetdFile = event.target.files[0];
+    const reader = new FileReader();
+
+    // Set uploading start/end handlers
+    let setFileProgress = this.setFileProgress.bind(this);
+    reader.onloadstart = function () {
+      setFileProgress(true);
+    };
+    reader.onloadend = function () {
+      // Load emulation
+      setTimeout(() => {
+        setFileProgress(false);
+      }, 1000);
+    };
+
+    // Set file to note
+    reader.onload = () => {
+      this.note.imagePreview = reader.result as string;
+    };
+    reader.readAsDataURL(this.selecetdFile);
+  }
+
+  // Outputing file upload start/end event
+  @Output() fileProgress = new EventEmitter<boolean>();
+  setFileProgress(show: boolean) {
+    this.fileProgress.emit(show);
+  }
+
 
 }
