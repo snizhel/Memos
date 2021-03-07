1d754edd8339d4e0c91086ab8ef24caa26927f6f
Thien <bananakind123@gmail.com>
2021-03-07 10:19:46 +0700

    add register

--- Memos/src/app/pages/sign-in/sign-in.module.ts
+++ Memos/src/app/pages/sign-in/sign-in.module.ts
@@ -6,13 +6,14 @@ import { SignInComponent } from './sign-in.component';
 import { environment } from 'src/environments/environment'
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth'
-
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [SignInComponent],
   imports: [
     CommonModule,
     SignInRoutingModule,
+    FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
 
